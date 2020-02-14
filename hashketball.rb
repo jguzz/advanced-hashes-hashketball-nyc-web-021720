8e80f551a40145723994f1c15ce9668c5f9a8c37
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 21:26:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -303,9 +303,9 @@ def long_name_steals_a_ton?
   longest_name = player_with_longest_name()
   most_points = most_points_scored()
   game_hash.each do |place,team|
-    team.each do |attributes, data|
+    team.each do |attributes, i|
       if attributes == :players
-        data.each do |player|
+        i.each do |player|
           # binding.pry
           if (player[:player_name] == longest_name) && (player[:points] == most_points)
             return true
