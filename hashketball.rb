c12e800866cd2a9edfb50afe0183770c16e8daf8
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 21:03:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -266,7 +266,7 @@ def winning_team
       end
     end
     #calculate total away points
-    if place == :away
+    elif place == :away
       team.each do |attribute, data|
         if attribute == :players
           data.each do |player|
