02ffc0816846f8c86f9c79d86ee21e32b354095d
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-14 20:41:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,3 +228,17 @@ def big_shoe_rebounds
   end
   rebounds
 end
+
+most_points_scored()
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:points]
+          end
+        end
+      end
+    end
+  end
+end
